9c9d067b99d68a3fa8009f66441b342e50d61fbf
DenisovaSS <114432399+DenisovaSS@users.noreply.github.com>
2023-08-13 15:50:29 +0300

    feat: add solve

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    const stNum = Math.abs(n).toString();
+    let revNum = "";
+    for (let i = stNum.length - 1; i >= 0; i--) {
+        revNum += stNum[i];
+    }
+    return +revNum;
+};
